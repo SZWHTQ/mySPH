b4c3e82f179a033c4ecadc78d4b96fcca09c442e
SZWHTQ <percivalric@icloud.com>
2023-04-28 17:23:25 +0800

    minor changes

--- tar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-./clean.sh --all
-name="mySPH_`date "+%Y%m%d-%H%M"`.tgz"
-cd ..
-tar --exclude doc/books/光滑粒子流体动力学：一种无网格粒子法.pdf \
-    --use-compress-program=pigz \
-    -pcvf ~/git/TimeMachine/$name ./mySPH
-chmod 755 ~/git/TimeMachine/$name
-cd ./mySPH

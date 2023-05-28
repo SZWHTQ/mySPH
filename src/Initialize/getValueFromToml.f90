eb4bff4c92edaa0b20bb9bf20533a151dce6884b
SZWHTQ <percivalric@icloud.com>
2023-05-28 16:48:53 +0800

    Support for windows

--- src/Initialize/getValueFromToml.f90
+++ src/Initialize/getValueFromToml.f90
@@ -64,8 +64,13 @@ contains
                 stop
             end if
         end select
+#ifndef _WIN32
         Project%out_path = Project%in_path//"/output"
         Project%vtk_path = Project%in_path//"/vtk"
+#elif
+    Project%out_path = Project%in_path//"\output"
+    Project%vtk_path = Project%in_path//"\vtk"
+#endif
 
         call create_directory(Project%out_path)
         call create_directory(Project%vtk_path)
